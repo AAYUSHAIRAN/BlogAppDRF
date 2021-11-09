62775f3dd1007dc6ea9144699a93ac8d2c477017
AAYUSHAIRAN <ayushairan799@gmail.com>
2021-11-09 20:09:34 +0530

    deploy

--- blogs/settings.py
+++ blogs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,5 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication',],
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny',],
 }
+
+django_heroku.settings(locals())
